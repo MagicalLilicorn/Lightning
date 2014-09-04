caec9bdaddd53d1d64d3fee7e8a6b7f4051eefae
Lily <lily4ever@msn.com>
2014-09-04 11:41:42 -0700

    Azula lightning

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,47 @@
-void setup()
-{
-  size(300,300);
-}
-void draw()
-{
+/* @pjs preload="azula.png"; */
+int startX = 275;
+int startY = 250;
+int endX = 275;
+int endY = 250;
+PImage azula;
+boolean firstTime = true;
+
+void setup(){
+	azula = loadImage("azula1.png");
+	size(600,600);
+	strokeWeight(1.5);
 
 }
-void mousePressed()
-{
 
+void background(){
+	image(azula, 0, 0, width, height);
+}
+void draw(){
+	int x = (int)random(0,150);
+	while(endY > 0){
+		stroke(7,63+x,110+x);
+		endX = startX + ((int)(Math.random()*18-8));
+		endY = startY - ((int)(Math.random()*10));
+	    line(startX,startY,endX,endY);
+	    drawRect();
+	}
+	startX = 275;
+	startY = 250;
+	endX = 275;
+	endY = 250;
+	if(firstTime == true){
+		firstTime=false;
+		background();
+	}
+	noStroke();
+	fill(0,0,0);
+	rect(10,500,225,75);
+}
+void drawRect(){
+	noStroke();
+    fill(70,7,7,5);
+    rect(0,0,600,250);
+    startX = endX;
+    startY = endY;
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,51 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-public void setup()
-{
-  size(300,300);
-  noLoop();
+/* @pjs preload="azula.png"; */
+int startX = 275;
+int startY = 250;
+int endX = 275;
+int endY = 250;
+PImage azula;
+boolean firstTime = true;
+
+public void setup(){
+	azula = loadImage("azula1.png");
+	size(600,600);
+	strokeWeight(1.5f);
+
 }
-public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+
+public void background(){
+	image(azula, 0, 0, width, height);
 }
-public void mousePressed()
-{
-  redraw();
+public void draw(){
+	int x = (int)random(0,150);
+	while(endY > 0){
+		stroke(7,63+x,110+x);
+		endX = startX + ((int)(Math.random()*18-8));
+		endY = startY - ((int)(Math.random()*10));
+	    line(startX,startY,endX,endY);
+	    drawRect();
+	}
+	startX = 275;
+	startY = 250;
+	endX = 275;
+	endY = 250;
+	if(firstTime == true){
+		firstTime=false;
+		background();
+	}
+	noStroke();
+	fill(0,0,0);
+	rect(10,500,225,75);
+}
+public void drawRect(){
+	noStroke();
+    fill(70,7,7,5);
+    rect(0,0,600,250);
+    startX = endX;
+    startY = endY;
 }
 
   static public void main(String[] passedArgs) {
