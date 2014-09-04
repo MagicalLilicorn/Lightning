e3ad30b63d773b12b8e54f212370c750aa348895
Lily <lily4ever@msn.com>
2014-09-04 12:13:14 -0700

    fixed stuffs

--- Lightning.pde
+++ Lightning.pde
@@ -1,47 +1,46 @@
 /* @pjs preload="azula.png"; */
-int startX = 275;
-int startY = 250;
-int endX = 275;
-int endY = 250;
 PImage azula;
-boolean firstTime = true;
 
 void setup(){
 	azula = loadImage("azula1.png");
 	size(600,600);
 	strokeWeight(1.5);
-
+	frameRate(15);
 }
 
-void background(){
-	image(azula, 0, 0, width, height);
-}
 void draw(){
-	int x = (int)random(0,150);
-	while(endY > 0){
-		stroke(7,63+x,110+x);
-		endX = startX + ((int)(Math.random()*18-8));
-		endY = startY - ((int)(Math.random()*10));
-	    line(startX,startY,endX,endY);
-	    drawRect();
+	image(azula, 0, 0, width, height);
+	tint(255,126);
+	for (int i = 0; i<5; i++){
+		Litening poop = new Litening();
+		poop.show();
 	}
-	startX = 275;
-	startY = 250;
-	endX = 275;
-	endY = 250;
-	if(firstTime == true){
-		firstTime=false;
-		background();
-	}
-	noStroke();
-	fill(0,0,0);
-	rect(10,500,225,75);
+
 }
-void drawRect(){
-	noStroke();
-    fill(70,7,7,5);
-    rect(0,0,600,250);
-    startX = endX;
-    startY = endY;
+
+class Litening {
+	int startX = 275;
+	int startY = 250;
+	int endX = 275;
+	int endY = 250;
+
+	void show(){
+		int x = (int)random(0,150);
+		while(endY > 0){
+			stroke(7,63+x,110+x);
+			endX = startX + ((int)(Math.random()*18-8));
+			endY = startY - ((int)(Math.random()*10));
+		    line(startX,startY,endX,endY);
+		    startX = endX;
+		    startY = endY;
+		}
+		startX = 275;
+		startY = 250;
+		endX = 275;
+		endY = 250;
+		noStroke();
+		fill(0,0,0);
+		rect(10,500,225,75);
+	}
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -15,50 +15,49 @@ import java.io.IOException;
 public class Lightning extends PApplet {
 
 /* @pjs preload="azula.png"; */
-int startX = 275;
-int startY = 250;
-int endX = 275;
-int endY = 250;
 PImage azula;
-boolean firstTime = true;
 
 public void setup(){
 	azula = loadImage("azula1.png");
 	size(600,600);
 	strokeWeight(1.5f);
-
+	frameRate(15);
 }
 
-public void background(){
-	image(azula, 0, 0, width, height);
-}
 public void draw(){
-	int x = (int)random(0,150);
-	while(endY > 0){
-		stroke(7,63+x,110+x);
-		endX = startX + ((int)(Math.random()*18-8));
-		endY = startY - ((int)(Math.random()*10));
-	    line(startX,startY,endX,endY);
-	    drawRect();
+	image(azula, 0, 0, width, height);
+	tint(255,126);
+	for (int i = 0; i<5; i++){
+		Litening poop = new Litening();
+		poop.show();
 	}
-	startX = 275;
-	startY = 250;
-	endX = 275;
-	endY = 250;
-	if(firstTime == true){
-		firstTime=false;
-		background();
-	}
-	noStroke();
-	fill(0,0,0);
-	rect(10,500,225,75);
+
 }
-public void drawRect(){
-	noStroke();
-    fill(70,7,7,5);
-    rect(0,0,600,250);
-    startX = endX;
-    startY = endY;
+
+class Litening {
+	int startX = 275;
+	int startY = 250;
+	int endX = 275;
+	int endY = 250;
+
+	public void show(){
+		int x = (int)random(0,150);
+		while(endY > 0){
+			stroke(7,63+x,110+x);
+			endX = startX + ((int)(Math.random()*18-8));
+			endY = startY - ((int)(Math.random()*10));
+		    line(startX,startY,endX,endY);
+		    startX = endX;
+		    startY = endY;
+		}
+		startX = 275;
+		startY = 250;
+		endX = 275;
+		endY = 250;
+		noStroke();
+		fill(0,0,0);
+		rect(10,500,225,75);
+	}
 }
 
   static public void main(String[] passedArgs) {
